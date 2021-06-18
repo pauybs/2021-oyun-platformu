0c19de215fcbc190d0218d6d0ebbed5f9fd2d6e4
tayyarozel <60115382+tayyarozel@users.noreply.github.com>
2021-06-19 01:44:30 +0300

    Create uyeliksozlesmesi.php

---
+++ uyeliksozlesmesi.php
@@ -0,0 +1,27 @@
+<?php
+require_once("settings/connect.php");
+if(basename($_SERVER['PHP_SELF'])!=basename(__FILE__)){
+?>
+<div class="col-12 p-0 mt-2" style="background:#e7e6e3; ">
+	<div class="wrapper">
+  		<div class="news-item standard-item2 " ></div>
+  		<div class="position-absolute  col-12 text-center "style="margin-top: 120px; "><span class="HakkimizdaYazi "  >Üyelik Sözleşmesi</span></div>
+	</div>
+
+	<div class="col-12 mt-5 mb-5">
+		<div class="row justify-content-center hakkimizda mt-5">
+			<div class="col-12 col-xl-10 HakkimizdaAciklama mt-5" style="opacity: 0.7">
+				<div><?php echo IcerikTemizle($UyelikSozlesme);?></div>
+			</div>
+		</div>
+	</div>
+
+
+	
+</div>
+<?php
+}else{
+    header("location:" .$SiteLink);
+    exit();
+}
+?>
