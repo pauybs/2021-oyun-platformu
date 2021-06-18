44a4e846242502d2b896e8781e22457f70aac00e
tayyarozel <60115382+tayyarozel@users.noreply.github.com>
2021-06-19 01:43:42 +0300

    Create gizlilik.php

---
+++ gizlilik.php
@@ -0,0 +1,24 @@
+<?php
+require_once("settings/connect.php");
+if(basename($_SERVER['PHP_SELF'])!=basename(__FILE__)){
+?>
+<div class="col-12 p-0 mt-2 " style="background:#e7e6e3;">
+	<div class="wrapper">
+		<div class="news-item standard-item " ></div>
+		<div class="position-absolute  col-12 text-center "style="margin-top: 120px; "><span class="HakkimizdaYazi " >Gizlilik Politikası</span></div>
+	</div>
+
+	<div class="col-12 mt-5 mb-5">
+		<div class="row justify-content-center hakkimizda mt-5">
+			<div class="col-12 col-xl-10  HakkimizdaAciklama mt-5" >
+				<div ><?php echo IcerikTemizle($GizlilikSozlesme);?></div>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+}else{
+    header("location:".$SiteLink);
+    exit();
+}
+?>
