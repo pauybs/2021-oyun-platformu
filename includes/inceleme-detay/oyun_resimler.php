4d24f29e3fcb156327f7e5c1c1a9ff3f3f9ae7e2
tayyarozel <60115382+tayyarozel@users.noreply.github.com>
2021-06-19 01:37:00 +0300

    Create oyun_resimler.php

---
+++ includes/inceleme-detay/oyun_resimler.php
@@ -0,0 +1,58 @@
+<?php
+if(basename($_SERVER['PHP_SELF'])!=basename(__FILE__)){
+?>
+	<?php
+	$OyunResimler= $DatabaseBaglanti->prepare("SELECT * FROM  oyunresimler WHERE  OyunId=? ");
+	$OyunResimler->execute([Guvenlik($OyunBilgilerKayit["id"])]);
+	$OyunResimlerVeri = $OyunResimler->rowCount();
+	$OyunResimlerKayitlar = $OyunResimler->fetchAll(PDO::FETCH_ASSOC);
+	if ($OyunResimlerVeri>0) {
+	?>
+	<div class="row justify-content-center p-0">
+		<div class="col-12 col-md-10 p-0">
+			<div class="swiper-container swiper6">
+	 				<div class="swiper-wrapper " >
+				<?php
+				foreach ($OyunResimlerKayitlar as  $Resimler) {
+				?>
+					<?php
+					if (IcerikTemizle($Resimler["Resim"]!="") and IcerikTemizle(isset($Resimler["Resim"])) and file_exists("images/games/pictures/".$Resimler["Resim"]) ) {
+					?>	
+						<div class="swiper-slide"> 
+							<img  src="images/games/pictures/<?php echo IcerikTemizle($Resimler["Resim"]); ?>" class="img-fluid " alt="<?php echo $OyunAdi; ?>"/>
+							
+						</div>
+					<?php
+					}else{
+					?>	
+						<div class="swiper-slide">
+							<img src="images/hata2.jpg" class="img-fluid "/>
+						</div>
+					<?php
+					}
+					?>
+				<?php
+				}
+				?>
+				</div>	 
+			    <div class="swiper-button-next"></div>
+			    <div class="swiper-button-prev"></div>
+			</div>
+		</div>
+	</div>
+	<?php
+	}
+	?>
+<?php
+}else{
+	require_once("../../settings/connect.php");
+
+	header("location:" .$SiteLink);
+  exit();
+}
+?>
+
+
+
+
+
