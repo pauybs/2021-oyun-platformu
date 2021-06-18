bdccf7347fca77115f93eba3c0104100bdd5773a
tayyarozel <60115382+tayyarozel@users.noreply.github.com>
2021-06-19 01:58:58 +0300

    Create AdminPagesPublic.php

---
+++ settings/AdminPagesPublic.php
@@ -0,0 +1,11 @@
+<?php
+//dışşş
+$PagesPublic[0]="anasayfa.php";
+$PagesPublic[1]="admin-giris.php";
+$PagesPublic[2]="admin-giris-sonuc.php";
+$PagesPublic[3]="admin-giris-sonuc-hata.php";
+$PagesPublic[4]="admin-cikis.php";
+
+
+
+?>
